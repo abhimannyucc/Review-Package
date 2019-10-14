c5b7f8e2bdf44be93aa2e2a035c214e3351ace11
josephbondoc <josephsantosbondoc@gmail.com>
2019-10-14 16:27:10 -0700

    Added Top
    sloppy toppy

---
+++ styles/Topstyles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+import {View,Text} from 'react-native';
+
+var styles = StyleSheet.create({
+    
+    app: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:"100%",
+        height:'100%'
+    },
+    night: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#262626',
+        width:"100%",
+        height:'100%'
+    }
+})
+
+export default styles; 
